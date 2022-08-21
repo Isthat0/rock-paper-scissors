b4f68a9e88bd6a208564deec807220cf38fb3eed
isthat0 <oc15@my.fsu.com>
2022-08-21 15:39:19 -0400

    More comments'

--- java.js
+++ java.js
@@ -7,6 +7,7 @@ function game(){
     let l = 0;
     let d = 0;
     for (let i = 0; i < 5 ; i++){
+        //Get computer choice
         function getComputerChoice(){
             let choices = ['ROCK', 'PAPER', 'SCISSORS'];
             let randomIndex = Math.floor(Math.random() * choices.length);
@@ -17,50 +18,52 @@ function game(){
         let youWin = "You win!";
         let youLose = "You lost";
         let youDraw = "Draw!";
-        // single round of rock paper scissors
-        function playRound(playerSelection, computerSelection) {
-        
-            
-            if (computerSelection === playerSelection){
-                return youDraw;
-            }
-            
-            else if (computerSelection === 'ROCK'){
-                if (playerSelection === 'SCISSORS'){
-                    return youLose;
-                }
-                else if (playerSelection === 'PAPER'){
-                    return youWin; 
-                }
-            }
-           
-            else if (computerSelection === 'SCISSORS'){
-                if (playerSelection === 'PAPER'){
-                    return youLose;   
-                }
-                else if (playerSelection === 'ROCK'){
-                    return youWin; 
-                }
-            }
-        
-            else {
-                if (playerSelection === 'SCISSORS'){
-                    return youLose;   
-                }
-                else if (playerSelection === 'ROCK'){
-                    return youWin; 
-                } 
-            }
-        
-        }
          // have the player select rock paper or scissors and get it to Uppercase.  
         let playerChoice = prompt('rock, paper, or scissors?')
         let playerSelection = playerChoice.toUpperCase();
         let computerSelection = getComputerChoice();
+        // single round of rock paper scissors
+        function playRound(playerSelection, computerSelection) {
+        
+            
+                    if (computerSelection === playerSelection){
+                        return youDraw;
+                    }
+                    
+                    else if (computerSelection === 'ROCK'){
+                        if (playerSelection === 'SCISSORS'){
+                            return youLose;
+                        }
+                        else if (playerSelection === 'PAPER'){
+                            return youWin; 
+                        }
+                    }
+                   
+                    else if (computerSelection === 'SCISSORS'){
+                        if (playerSelection === 'PAPER'){
+                            return youLose;   
+                        }
+                        else if (playerSelection === 'ROCK'){
+                            return youWin; 
+                        }
+                    }
+                
+                    else {
+                        if (playerSelection === 'SCISSORS'){
+                            return youLose;   
+                        }
+                        else if (playerSelection === 'ROCK'){
+                            return youWin; 
+                        } 
+                    }
+                
+        }
+        //showing the what the person and computer chose and who won the round
         console.log(computerSelection);
         console.log(playerSelection);
         let round = playRound(playerSelection, computerSelection);
         console.log(round);
+        //track how many wins, draws, and loses
         if(round === youDraw){
             d++;
         }
