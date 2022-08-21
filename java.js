58a038b5b38b5aec852be93ac476780f4bd4b275
isthat0 <oc15@my.fsu.com>
2022-08-21 15:22:46 -0400

    Finish basic game.

--- java.js
+++ java.js
@@ -1,66 +1,66 @@
 // create array of choices
 // randomly pick rock paper, scissors.
-function getComputerChoice(){
-    let choices = ['ROCK', 'PAPER', 'SCISSORS'];
-    let randomIndex = Math.floor(Math.random() * choices.length);
-    let randomOption = choices[randomIndex];
-    return randomOption;
-}
 
-let youWin = "You win!";
-let youLose = "You lost";
-let youDraw = "Draw!";
-// single round of rock paper scissors
-function playRound(playerSelection, computerSelection) {
 
-    
-    if (computerSelection === playerSelection){
-        return youDraw;
-    }
-    
-    else if (computerSelection === 'ROCK'){
-        if (playerSelection === 'SCISSORS'){
-            return youLose;
-        }
-        else if (playerSelection === 'PAPER'){
-            return youWin; 
-        }
-    }
-   
-    else if (computerSelection === 'SCISSORS'){
-        if (playerSelection === 'PAPER'){
-            return youLose;   
-        }
-        else if (playerSelection === 'ROCK'){
-            return youWin; 
-        }
-    }
-
-    else {
-        if (playerSelection === 'SCISSORS'){
-            return youLose;   
-        }
-        else if (playerSelection === 'ROCK'){
-            return youWin; 
-        } 
-    }
-
-}
- // have the player select rock paper or scissors and get it to Uppercase.  
-let playerChoice = prompt('rock, paper, or scissors?')
-let playerSelection = playerChoice.toUpperCase();
-let computerSelection = getComputerChoice();
-console.log(computerSelection);
-console.log(playerSelection);
-console.log(playRound(playerSelection, computerSelection));
-
-let round = playRound(playerSelection,computerSelection);
-
-function game(round){
+function game(){
     let w = 0;
     let l = 0;
     let d = 0;
     for (let i = 0; i < 5 ; i++){
+        function getComputerChoice(){
+            let choices = ['ROCK', 'PAPER', 'SCISSORS'];
+            let randomIndex = Math.floor(Math.random() * choices.length);
+            let randomOption = choices[randomIndex];
+            return randomOption;
+        }
+        
+        let youWin = "You win!";
+        let youLose = "You lost";
+        let youDraw = "Draw!";
+        // single round of rock paper scissors
+        function playRound(playerSelection, computerSelection) {
+        
+            
+            if (computerSelection === playerSelection){
+                return youDraw;
+            }
+            
+            else if (computerSelection === 'ROCK'){
+                if (playerSelection === 'SCISSORS'){
+                    return youLose;
+                }
+                else if (playerSelection === 'PAPER'){
+                    return youWin; 
+                }
+            }
+           
+            else if (computerSelection === 'SCISSORS'){
+                if (playerSelection === 'PAPER'){
+                    return youLose;   
+                }
+                else if (playerSelection === 'ROCK'){
+                    return youWin; 
+                }
+            }
+        
+            else {
+                if (playerSelection === 'SCISSORS'){
+                    return youLose;   
+                }
+                else if (playerSelection === 'ROCK'){
+                    return youWin; 
+                } 
+            }
+        
+        }
+         // have the player select rock paper or scissors and get it to Uppercase.  
+        let playerChoice = prompt('rock, paper, or scissors?')
+        let playerSelection = playerChoice.toUpperCase();
+        let computerSelection = getComputerChoice();
+        console.log(computerSelection);
+        console.log(playerSelection);
+        let round = playRound(playerSelection, computerSelection);
+        console.log(round);
         if(round === youDraw){
             d++;
         }
@@ -70,7 +70,16 @@ function game(round){
         else{
             l++;
         }
-    }    
+    }
+    if (w > l){
+        console.log("You are the winner!");
+    }
+    else if (l > w){
+        console.log("You are the loser :(");
+    }
+    else{
+        console.log ("It's a tie!");
+    }
 }
-console.log(round);
+
 game();
